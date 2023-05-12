e0cadd45115e7c991cad6d6b4f53117564fb70aa
havertzz04 <havertzz04@gmail.com>
2023-05-12 08:50:19 -0500

    feat: :art: Remove deleted functions

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-import { addUser } from "./components/api.js"
 import { Form } from "./htmlComponents/Form.js";
 import { Table } from "./htmlComponents/Table.js";
-import { Music } from "./htmlComponents/Music.js";
-import { music } from "./components/music.js";
 import logic from "./components/logic.js";
 
-music();
 logic();
